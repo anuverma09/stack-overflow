a8a9e07f3a9719d8f2cc8b339a3727518459be64
anuverma09 <anu.verma09@gmail.com>
2024-06-04 18:23:04 +0530

    webhook commit1

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,15 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/ask-question(.*)"]);
+const isProtectedRoute = createRouteMatcher([
+  "/ask-question(.*)",
+  "/",
+  "/api/webhook",
+  "/tags",
+  "/tags/:id",
+  "/profile/:id",
+  "/community",
+  "/jobs",
+]);
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
 });
@@ -8,3 +17,23 @@ export default clerkMiddleware((auth, req) => {
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+// export default clerkMiddleware({
+//   publicRoutes: [
+//     "/",
+//     "/api/webhook",
+//     "/question/:id",
+//     "/tags",
+//     "/tags/:id",
+//     "/profile/:id",
+//     "/community",
+//     "/jobs",
+//   ],
+//   ignoredRoutes: ["/api/webhook", "/api/chatgpt"],
+// });
+
+// export const config = {
+//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
